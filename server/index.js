fb9fcea99d35e36e4cb6607e6078573d58bae8a1
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2025-03-02 00:01:01 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const {config}=require("dotenv")
+config()
+const express=require("express")
+const app=express()
+const {Server}=require("socket.io")
+const { createServer } = require("http");
+const cors=require("cors")
+app.use(cors({
+    origin:"*"
+}))
+const httpServer=createServer(app)
+const io=new Server(httpServer,{
+    cors:{
+        origin:"*"
+    }
+})
+io.on("connection",(socket)=>{
+    console.log("new socket connected",socket)
+})
+const port=process.env.PORT
+httpServer.listen(port,()=>{
+    console.log(`app is listening at the http://localhost:${port}`)
+})
